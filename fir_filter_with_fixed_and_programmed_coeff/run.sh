69daec8e1a0ef86a45bf212872248cd1005ebf5a
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-02-25 17:54:00 +0100

    The second fir architecture has been added

--- fir_filter_with_fixed_and_programmed_coeff/run.sh
+++ fir_filter_with_fixed_and_programmed_coeff/run.sh
@@ -19,17 +19,6 @@ build/config_rtl_chain_arranged_adders.exe --fst=waves/waves_config_rtl_chain_ar
 echo "Opening waveform viewer (only is the fst file exists)"
 [ -f waves/waves_config_rtl_chain_arranged_adders.fst ] && gtkwave waves/waves_config_rtl_chain_arranged_adders.fst &
 
-
-echo "Elaborating three_arranged_adders testbench"
-ghdl -e -fsynopsys --workdir=build -o build/config_rtl_three_arranged_adders config_rtl_three_arranged_adders
-
-echo "Running config_rtl_three_arranged_adders testbench. Waveforms saved to waves/waves_config_rtl_three_arranged_adders.fst"
-build/config_rtl_three_arranged_adders.exe --fst=waves/waves_config_rtl_three_arranged_adders.fst
-
-echo "Opening waveform viewer for config_rtl_three_arranged_adders (only if the fst file exists)"
-[ -f waves/waves_config_rtl_three_arranged_adders.fst ] && gtkwave waves/waves_config_rtl_three_arranged_adders.fst &
-
-
 echo "Elaborating pipeline_arranged_adders testbench"
 ghdl -e -fsynopsys --workdir=build -o build/config_rtl_pipeline_arranged_adders config_rtl_pipeline_arranged_adders
 
