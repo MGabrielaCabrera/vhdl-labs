cc18d156c60d16158d31a131a7e2f958622cb49a
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-03-25 19:26:34 +0100

    Modify the tb location in leading_ones_counter_comb test

--- leading_ones_counter_comb/run.sh
+++ leading_ones_counter_comb/run.sh
@@ -5,6 +5,7 @@ mkdir -p build waves
 
 echo "Analyzing design files"
 ghdl -a -fsynopsys --workdir=build *.vhd
+ghdl -a -fsynopsys --workdir=build tb/*.vhd
 
 echo "Elaborating config_rtl testbench"
 ghdl -e -fsynopsys --workdir=build -o build/config_rtl config_rtl
