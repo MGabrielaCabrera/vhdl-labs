4db2e48bc6469f05c31183e6268383b4f4a35a19
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-04-16 16:09:41 +0200

    Scoreboard implemented simplifying the error reports

---
+++ axi_lite_fir_dsp_peripheral/tb/sv/classes/scoreboard.sv
@@ -0,0 +1,34 @@
+// Simple scoreboard class for tracking test results
+
+class Scoreboard;
+  int fail_count = 0;
+  int fail_count_prev = 0;
+
+  task check(string name, bit cond, string msg);
+    assert (cond) else begin
+      $error("[FAIL] %s: %s", name, msg);
+      fail_count++;
+    end
+  endtask
+
+  function init_test(string test_name);
+    $display("[%0t ns] %s", $realtime, test_name);
+
+    fail_count_prev = fail_count; // Save current fail count to track new failures
+  endfunction
+
+  function test_report(string test_name);
+    if (fail_count - fail_count_prev == 0)
+      $display("%s PASSED", test_name);
+    else
+      $display("%s FAILED: %0d errors", test_name, fail_count - fail_count_prev);
+  endfunction
+
+
+  function void general_report();
+    if (fail_count == 0)
+      $display("ALL TESTS PASSED");
+    else
+      $display("FAILED: %0d errors", fail_count);
+  endfunction
+endclass
